60b54b67838732de8339c1c85a42ffc2e6ab2aaa
Paula <94paulala@gmail.com>
2025-08-04 11:17:53 +0200

    gsap installed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
+import { Component, AfterViewInit, NgZone, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Navbar } from './components/navbar/navbar';
@@ -11,8 +10,15 @@ import { Faq } from './components/faq/faq';
 import { Contact } from './components/contact/contact';
 import { Footer } from './components/footer/footer';
 
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import ScrollSmoother from 'gsap/ScrollSmoother';
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [
     CommonModule,
     Navbar,
@@ -27,6 +33,25 @@ import { Footer } from './components/footer/footer';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements AfterViewInit, OnDestroy {
+  private smoother: ScrollSmoother | undefined;
   protected title = 'paulala';
+
+  constructor(private zone: NgZone) {}
+
+  ngAfterViewInit() {
+    this.zone.runOutsideAngular(() => {
+      this.smoother = ScrollSmoother.create({
+        wrapper: '#smooth-wrapper',
+        content: '#smooth-content',
+        smooth: 1.2,
+        effects: true,
+        normalizeScroll: true,
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    this.smoother?.kill();
+  }
 }
--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Button } from '../button/button';
+
 import projectsData from '../../../assets/projects.json';
 
 @Component({
@@ -9,8 +10,21 @@ import projectsData from '../../../assets/projects.json';
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
-export class Projects {
+export class Projects implements AfterViewInit {
   projects = projectsData;
+
+  ngAfterViewInit(): void {
+    gsap.to('#projects', {
+      backgroundPosition: '50% 30%', // Ejemplo: mueve fondo del centro al 30% verticalmente
+      ease: 'none',
+      scrollTrigger: {
+        trigger: '#projects',
+        start: 'top bottom', // Cuando la sección empieza a entrar
+        end: 'bottom top', // Hasta que la sección sale de la pantalla
+        scrub: true, // Animación sincronizada con el scroll
+      },
+    });
+  }
 
   navigateToProject(url: string): void {
     if (this.isValidUrl(url)) {
