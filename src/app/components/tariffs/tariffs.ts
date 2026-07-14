18e252e519ea138ba0811e54fb07fb06786a9973
Paulala Luengo <you@example.com>
2026-07-14 20:10:16 +0200

    new changes done

--- src/app/components/tariffs/tariffs.ts
+++ src/app/components/tariffs/tariffs.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Button } from '../button/button';
 import { PricingModal } from '../pricing-modal/pricing-modal';
@@ -10,40 +10,10 @@ import { PricingModal } from '../pricing-modal/pricing-modal';
   templateUrl: './tariffs.html',
   styleUrl: './tariffs.scss',
 })
-export class Tariffs implements AfterViewInit {
-  hoveredIndex: number | null = null;
-
+export class Tariffs {
   showModal = false;
   selectedPlan: string | null = null;
-
-  constructor(private elementRef: ElementRef) {}
-
-  ngAfterViewInit(): void {
-    const cards = Array.from(
-      this.elementRef.nativeElement.querySelectorAll('.pricing-cards .card')
-    ) as HTMLElement[];
-    const delayStep = 300;
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            cards.forEach((card, i) => {
-              setTimeout(() => {
-                card.classList.add('in-view');
-              }, i * delayStep);
-            });
-            observer.disconnect();
-          }
-        });
-      },
-      { threshold: 0.3 }
-    );
-
-    if (cards.length) {
-      observer.observe(cards[0]);
-    }
-  }
+  hoveredIndex: number | null = null;
 
   openPricingForm(plan: string): void {
     this.selectedPlan = plan;
